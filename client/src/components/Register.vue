d1c3c967c10f3f9921336814b937302929357469
Rostislav Kral <Aiphyth@seznam.cz>
2020-04-10 23:43:56 +0200

    Client Api and backend setup

---
+++ client/src/components/Register.vue
@@ -0,0 +1,46 @@
+<template>
+ <div>
+   <h1>Register</h1>
+
+   <input
+     type="text"
+     name="email"
+     v-model="email"
+     placeholder="email"
+   >
+   <br>
+   <input
+     type="text"
+     name="password"
+     v-model="password"
+     placeholder="password"
+   >
+   <br>
+   <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      // eslint-disable-next-line no-unused-vars
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
